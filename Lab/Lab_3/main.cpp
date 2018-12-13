10b34600193684e570d102bfe4ad235917d90a95
nr2707842 <45056028+nr2707842@users.noreply.github.com>
2018-12-12 21:15:45 -0800

    Add files via upload

---
+++ Lab/Lab_3/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Nadia Rahbany
+ * Created on September 27, 2018, 9:24 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+const unsigned char PERCENT=100;
+/* Calculating the percentage of gas tax on a gallon of gas.*/
+int main(int argc, char** argv) {
+const unsigned char TAX=7;
+
+double 
+cout << "  The amount it cost you per gallon the last time you put gas in your car: $" << division << " million " << endl << endl;
+system("pause");
+return 0;
+}
